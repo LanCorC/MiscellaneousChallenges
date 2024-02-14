b822268858ad3253ec5ee2d29806002b359e0b7f
LanCorC <lanc3.corcega@gmail.com>
2024-02-15 10:58:12 +1300

    Two-Sum: optimized attempt, after reviewing some solutions

--- src/TwoSum/Main.java
+++ src/TwoSum/Main.java
@@ -9,17 +9,29 @@ public class Main {
         System.out.println(answer[0] + " " + answer[1]);
     }
     public static int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> seen = new HashMap<>();
-        int diff;
-        for (int i = 0; i< nums.length; i++) {
-            diff = target - nums[i];
-            if (seen.get(diff) != null) {
-                return new int[]{seen.get(diff), i};
-            } else {
-                seen.put(nums[i], i);
+//        HashMap<Integer,Integer> seen = new HashMap<>();
+//        int diff;
+//        for (int i = 0; i< nums.length; i++) {
+//            diff = target - nums[i];
+//            if (seen.get(diff) != null) {
+//                return new int[]{seen.get(diff), i};
+//            } else {
+//                seen.put(nums[i], i);
+//            }
+//        }
+//        return new int[]{9, 9};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) != null) {
+                result[0] = map.get(nums[i]);
+                result[1] = i;
+                break;
             }
+            map.put(target-nums[i], i);
         }
-        return new int[]{9, 9};
+        return result;
     }
 
 }
